596a0773aa82310c6fde364c07202cbd98c201d9
Sonya Slegers <sonjay08@gmail.com>
2018-07-06 15:25:34 -0500

    merged with limit-t-8-choices

--- src/components/box-contents.js
+++ src/components/box-contents.js
@@ -7,17 +7,6 @@ export class BoxContents extends React.Component {
 
   render() {
    const vegetableList = [];
-<<<<<<< HEAD
-   console.log('box.box-content state:', this.props.savedBoxContents);
-   console.log(vegetableList, this.props.unsavedBoxContents);
-   if (this.props.savedBoxContents) {
-    for (let i = 0; i < this.props.savedBoxContents.length; i++) {
-      vegetableList.push(<li key={i} className='box-content'>{this.props.savedBoxContents[i]}</li>)
-   }
-   } else {
-     for (let i = 0; i < this.props.unsavedBoxContents.length; i++) {
-       vegetableList.push(<li key={i} className='added-vegetable'>{this.props.unsavedBoxContents[i]}</li>)
-=======
    console.log('savedBoxContents:', this.props.savedBoxContents);
    console.log('rendering box-content');
    if (this.props.savedBoxContents) {
@@ -29,7 +18,6 @@ export class BoxContents extends React.Component {
      for (let i = 0; i < this.props.unsavedBoxContents.length; i++) {
        console.log('rendering unsavedBoxContents: ', this.props.unsavedBoxContents);
        vegetableList.push(<li key={i} className='added-vegetable'>{this.props.unsavedBoxContents[i]}</li>);
->>>>>>> limit-t-8-choices
      }
    }
    
